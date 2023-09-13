719ea985208ea2340db6939f51ae8bc9c70b2773
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-09-13 20:51:31 +0530

    Create maximumContiguousSubarraySum.java

---
+++ Algorithms/Kadanes/maximumContiguousSubarraySum.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+/*
+Given an array, A of length n.
+Your task is to find the contiguous subarray within A which has the largest sum.
+
+Input Format
+The first line of each test case contains an integer n, the length of the array A.
+The next line contains n integers.
+
+Output Format
+Output an integer representing the maximum possible sum of the contiguous subarray.
+
+Example 1
+Input
+5
+1 2 3 4 -10
+
+Output
+10
+
+Explanation
+1 + 2 + 3 + 4 = 10
+
+Example 2
+Input
+6
+2 -1 0 1 2 1
+
+Output
+5
+
+Explanation
+The sum of the entire array i.e 5.
+
+Constraints
+1 <= N <= 10^5
+-10^6 <= A[i] <= 10^6
+*/
+
+public class maximumContiguousSubarraySum {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = input.nextInt();
+        }
+        Solution ob =new Solution();
+        long ans = ob.maxSubarraySum(a,n);
+        System.out.println(ans);
+    }
+}
+class Solution{
+    public long maxSubarraySum(int a[],int n){
+        long max = Integer.MIN_VALUE;
+
+		long current = a[0];
+		//iterate the array
+		for(int index=1; index< a.length; index++){
+			
+			if(a[index] + current >= a[index]){
+				current+=a[index];
+			}else{
+				current = a[index];
+			}
+			
+			max = Math.max(max, current);
+		}
+
+		return max;
+    }
+}
